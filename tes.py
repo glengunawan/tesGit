fd439f63b632059fd571bc22d7b962929890a0f1
glengunawan <glen.putranto@gmail.com>
2020-02-13 10:50:16 +0700

    Ini commit keduaku

--- tes.py
+++ tes.py
@@ -5,6 +5,17 @@ print("x")
 print("x") 
 
 '''
-# Checkpoint
-0. pastikan di repo project udah git enabled (declare di folder(terminal) --> git init) --> git status untuk check
+# Checkpoint (bikin commit)
+0. pastikan di repo project udah git enabled (declare di folder(terminal) --> git init) 
+--> git status untuk check
+
+1. add files to staging area (declare file apa aja yang mau dibawa ke staging area) 
+git add <filename> --> 1 file 
+git add . --> all files 
+git rm --chached <filename> 
+
+2. bikin checkpoint 
+git commit --> masuk ke code editor vm (virtual machine) 
+git commit -m "Ini commit pertamaku --> message 
+
 '''
