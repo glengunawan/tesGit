9e15a6735c1a502283d44f9391d91dc7d9ce0fb6
glengunawan <glen.putranto@gmail.com>
2020-02-13 10:48:49 +0700

    Ini commit pertamaku

---
+++ tes.py
@@ -0,0 +1,10 @@
+print("x")
+print("x")
+print("x")
+print("x")
+print("x") 
+
+'''
+# Checkpoint
+0. pastikan di repo project udah git enabled (declare di folder(terminal) --> git init) --> git status untuk check
+'''
